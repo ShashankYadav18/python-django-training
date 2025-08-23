d7efa7a34fa1ee1616782587debd68d163c7ca6e
Shashankyadaav <shashanky485@gmail.com>
2025-08-23 14:03:14 +0530

    add

---
+++ 14-08-25/module.py
@@ -0,0 +1,21 @@
+# basic import
+
+import math
+print(math.factorial(5))
+
+#print(dir(math))
+#print(help(math))
+
+#from math import pi, sqrt
+
+#from math import *
+
+
+
+import file 
+
+print(file.add(4,5))
+print(file.sub(4,5))
+print(file.mul(4,5))
+print(file.div(4,5))
+
